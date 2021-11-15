156b3670e923b098b547be7596d20ae395cd30ea
coloursofnoise <coloursofthenoise@gmail.com>
2021-11-14 18:29:46 -0800

    Add mod name verification

--- init.sh
+++ init.sh
@@ -3,11 +3,27 @@ NAMEPATTERN='[^[:alnum:] _-]'
 while [ -z $MODNAME ]; do
     echo Enter Mod Name:
     read MODNAME
+
+    # skip verification
+    #break
+
     if [[ $MODNAME =~ $NAMEPATTERN ]]; then
         echo "!!! Name contains invalid characters.
 Valid characters include \`a-zA-Z0-9 _-\`"
         unset MODNAME
     fi
+
+    echo "Checking for duplicates..."
+    if [ -z "$UPDATELIST" ]; then
+        UPDATELIST=$(curl -s $(curl -s https://everestapi.github.io/modupdater.txt))
+    fi
+    for MOD in $UPDATELIST; do
+        if [[ $MODNAME: == $MOD ]]; then
+            echo "Name already in use!"
+            unset MODNAME
+            break
+        fi
+    done
 done
 
 find $(dirname "$0") -type f -not -path '*/[@.]*' -exec sed -i -e "s/\\\$MODNAME\\\$/$MODNAME/g" {} \;
